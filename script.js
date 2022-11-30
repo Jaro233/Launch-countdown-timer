3cc3e091611f8f976a2557cfa04fdfccb0e1e496
Jaro233 <jaroslaw.waliczek@vp.pl>
2022-11-30 14:14:57 +0100

    changed clock to flip.js plugin

--- script.js
+++ script.js
@@ -1,103 +1,21 @@
-var countDownDate = new Date("Jan 5, 2023 05:38:00").getTime();
-let second = 0;
-// Update the count down every 1 second
-var x = setInterval(function() {
 
-  // Get today's date and time
-  var now = new Date().getTime();
+function setupFlip(tick) {
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  Tick.helper.interval(function() {
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      tick.value++;
 
-  document.querySelector(".days_number").innerHTML = days;
-  document.querySelector(".hours_number").innerHTML = hours;
-  document.querySelector(".minutes_number").innerHTML = minutes;
-  document.querySelector(".seconds_number").innerHTML = seconds;   
-  
-  if(document.querySelector(".days_number").innerHTML < 10) {
-    document.querySelector(".days_number").innerHTML = `0${days}`
-  }
-  
-  if(document.querySelector(".hours_number").innerHTML < 10) {
-    document.querySelector(".hours_number").innerHTML = `0${hours}`
-  }
+      // Set `aria-label` attribute which screenreaders will read instead of HTML content
+      tick.root.setAttribute('aria-label', tick.value);
 
-  if(document.querySelector(".minutes_number").innerHTML < 10) {
-    document.querySelector(".minutes_number").innerHTML = `0${minutes}`
-  }
+  }, 1000);
 
-  if(document.querySelector(".seconds_number").innerHTML < 10) {
-    document.querySelector(".seconds_number").innerHTML = `0${seconds}`
-  }
-  
-  const rotate = (id) => {
-    document.getElementById(id).style.transform = "rotateX(90deg)";
+}
+
+function handleTickInit(tick) {
+  var nextYear = (new Date()).getFullYear() + 1;
+
+  Tick.count.down(nextYear + '-01-01').onupdate = function(value) {
+    tick.value = value;
   };
-  
-  const rotateDefault = (id) => {
-    document.getElementById(id).style.transform = "rotateX(360deg)";
-  };
-  
-  rotate("seconds_id");
-  const delay = setTimeout(() => {
-    rotateDefault("seconds_id");
-  }, 200);
-
-  //minutes
-  if (document.querySelector(".seconds_number").innerHTML <= 0) {
-    const delay = setTimeout(() => {
-      rotate("minutes_id");
-    }, 1000);
-    
-    const delay1 = setTimeout(() => {
-      rotateDefault("minutes_id");
-    }, 1200);
-    if (distance <= 0) {
-      clearTimeout(delay);
-      clearTimeout(delay1);
-    }
-  }
-  //hours
-  if (document.querySelector(".minutes_number").innerHTML <= 0){
-    const delay = setTimeout(() => {
-      rotate("hours_id");
-    }, 1000);
-    
-    const delay1 = setTimeout(() => {
-      rotateDefault("hours_id");
-    }, 1200);
-    if (distance <= 0) {
-      clearTimeout(delay);
-      clearTimeout(delay1);
-    }
-  }
-  //days
-  if (document.querySelector(".hours_number").innerHTML <= 0) {
-    const delay = setTimeout(() => {
-      rotate("days_id");
-    }, 1000);
-    
-    const delay1 = setTimeout(() => {
-      rotateDefault("days_id");
-    }, 1200);
-    if (distance <= 0) {
-      clearTimeout(delay);
-      clearTimeout(delay1);
-    }
-  }
-
-  console.log(second)
-
-  second++;
-  distance -= 1000;
-  if (distance <= 0) {
-    clearInterval(x);
-  }
-}, 1000);
-
+}
